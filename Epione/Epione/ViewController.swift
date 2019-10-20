55f6f411fd45e3f6f7f92df19ec873ea3c7e030e
Liem Thanh Ho <lthpkh@mst.edu>
2019-10-20 00:41:31 -0700

    uploading simpler xcode starting point

--- Epione/Epione/ViewController.swift
+++ Epione/Epione/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  ViewController.swift
 //  Epione
 //
 //  Created by Liem Thanh Ho on 10/20/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
