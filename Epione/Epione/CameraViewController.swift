8ba95e18a75184c0734b709abaf2a7a0bf599997
Liem Thanh Ho <lthpkh@mst.edu>
2019-10-20 12:52:37 -0700

    added app without camera acccess

---
+++ Epione/Epione/CameraViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CameraViewController.swift
+//  Epione
+//
+//  Created by Liem Thanh Ho on 10/20/19.
+//  Copyright © 2019 Health++. All rights reserved.
+//
+
+import UIKit
+
+
+
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    // Do any additional setup after loading the view.
+    
+    
+    @IBAction func lastButton(_ sender: Any) {performSegue(withIdentifier: "lastSegue", sender: self)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageView.image = info[.originalImage] as? UIImage
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Epione/Epione/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Epione
+//
+//  Created by Liem Thanh Ho on 10/20/19.
+//  Copyright © 2019 Health++. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBAction func cameraButton(_ sender: Any) { performSegue(withIdentifier: "cameraSegue", sender: self)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
