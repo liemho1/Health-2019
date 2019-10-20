680b775a1852f629912a5ef074259ba7880b2f9b
Liem Thanh Ho <lthpkh@mst.edu>
2019-10-20 13:08:26 -0700

    uploading project with buggy camera

--- Epione/Epione/CameraViewController.swift
+++ Epione/Epione/CameraViewController.swift
@@ -13,6 +13,7 @@ import UIKit
 class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var imagePicker: UIImagePickerController!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,14 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
     
     
     @IBAction func lastButton(_ sender: Any) {performSegue(withIdentifier: "lastSegue", sender: self)
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+
+        present(imagePicker, animated: true, completion: nil)
     }
+    
+
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
